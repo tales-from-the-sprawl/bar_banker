efd39d55da1fd6a28548af1168117f609730a663
Carl Malmgren <callem217@gmail.com>
2026-06-09 10:13:37 +0200

    whoops no db

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -31,8 +31,7 @@ defmodule BarBankerWeb.ConnCase do
     end
   end
 
-  setup tags do
-    BarBanker.DataCase.setup_sandbox(tags)
+  setup _tags do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
