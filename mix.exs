1ddfc44d95e7d0fc1a405ce2c4912b4057f02031
Carl Malmgren <callem217@gmail.com>
2026-06-10 21:53:25 +0200

    update liveview

--- mix.exs
+++ mix.exs
@@ -131,7 +131,7 @@ defmodule BarBanker.MixProject do
       {:phoenix, "~> 1.8.5"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.6", only: :dev},
-      {:phoenix_live_view, "~> 1.1.30"},
+      {:phoenix_live_view, "~> 1.2.0"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
